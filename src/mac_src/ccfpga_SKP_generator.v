b0aafd42ae93c8da1832b3f90e62d2de4b87f6a3
Luong, Vinh Hai <hai.luong@rwth-aachen.de>
2025-12-08 13:51:34 +0100

    Update generating SKP Ordered Set. Allows incrementing and decrementing of number of queued SKPs at the same time. Adds support for 16 and 8-Bit datapath

--- src/mac_src/ccfpga_SKP_generator.v
+++ src/mac_src/ccfpga_SKP_generator.v
@@ -24,18 +24,19 @@ module ccfpga_SKP_generator #(
     localparam COUNT_MAX   = (1538 - 1)/DATA_BYTES + 1;
     localparam COUNT_WIDTH = $clog2(COUNT_MAX) + 1;
 
-    reg [3:0] number_of_queued_SKP;
+    reg             [3:0] number_of_queued_SKP;
     reg [COUNT_WIDTH-1:0] s_count;
 
     wire SKP_count_inc;
     wire SKP_send_enable;
 
-    localparam COM  = 8'hBC; // K28.5
-    localparam SKP  = 8'h1C; // K28.0
+    localparam  COM    = 8'hBC; // K28.5
+    localparam  SKP    = 8'h1C; // K28.0
+    wire [31:0] SKP_OS = {SKP, SKP, SKP, COM}; // SKP Ordered Set
 
     always@(posedge clk or posedge reset) begin
         if (reset) begin
-            s_count <= {COUNT_WIDTH{1'b0}};
+            s_count     <= {COUNT_WIDTH{1'b0}};
         end
         else begin
             if (s_count == COUNT_MAX) begin
@@ -47,65 +48,104 @@ module ccfpga_SKP_generator #(
         end
     end
 
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            number_of_queued_SKP <= 4'b0;
-        end else if (SKP_send_enable) begin
-            if (number_of_queued_SKP >= DATA_WIDTH/SKP_WIDTH) begin
-                number_of_queued_SKP <= number_of_queued_SKP - DATA_WIDTH/SKP_WIDTH;
-            end else begin
-                number_of_queued_SKP <= 4'b0;
-            end
-        end else if (SKP_count_inc == 1'b1) begin
-            number_of_queued_SKP <= number_of_queued_SKP + 1;
-        end
-    end
-
     generate
         if (DATA_WIDTH == 64) begin
             always @(posedge clk or posedge reset) begin
                 if (reset) begin
-                    txdata <= {DATA_WIDTH{1'b0}};
-                    txdatak <= {DATA_BYTES{1'b0}};
-                    sending_SKP <= 1'b0;
+                    txdata               <= {DATA_WIDTH{1'b0}};
+                    txdatak              <= {DATA_BYTES{1'b0}};
+                    sending_SKP          <= 1'b0;
+                    number_of_queued_SKP <= 4'b0;
                 end else if (SKP_send_enable) begin
                     if (number_of_queued_SKP >= DATA_WIDTH/SKP_WIDTH) begin
-                        // Send 2 SKP OS at once if more than 1 are queued
-                        txdata <= {SKP, SKP, SKP, COM, SKP, SKP, SKP, COM};
-                        txdatak <= 8'b11111111;
-                        sending_SKP <= 1'b1;
+                        // Send 2 SKP OSs at once if more than 1 are queued
+                        // Subtract only 1 SKP from the queue as one is added
+                        txdata               <= {SKP_OS, SKP_OS};
+                        txdatak              <= {DATA_BYTES{1'b1}};
+                        sending_SKP          <= 1'b1;
+                        if (SKP_count_inc)
+                            number_of_queued_SKP <= number_of_queued_SKP - 1'b1;
+                        else
+                            number_of_queued_SKP <= number_of_queued_SKP - DATA_WIDTH/SKP_WIDTH;
+                    end else if (number_of_queued_SKP == 1) begin
+                        // Send 2 SKP OSs at once if one SKP is queued and one is added
+                        if (SKP_count_inc) begin
+                            txdata               <= {SKP_OS, SKP_OS};
+                            txdatak              <= {DATA_BYTES{1'b1}};
+                        end else begin
+                            txdata               <= {32'h00000000, SKP_OS};
+                            txdatak              <= 8'b00001111;
+                        end
+                        sending_SKP          <= 1'b1;
+                        number_of_queued_SKP <= 4'b0;
+                    end else if (number_of_queued_SKP == 0 && SKP_count_inc) begin
+                        // Send 1 SKP OS with IDLE data
+                        txdata               <= {32'h00000000, SKP_OS};
+                        txdatak              <= 8'b00001111;
+                        sending_SKP          <= 1'b1;
+                        number_of_queued_SKP <= 4'b0;
                     end else begin
-                        // Send 1 SKP OS with IDLE data otherwise
-                        txdata <= {8'h00, 8'h00, 8'h00, 8'h00, SKP, SKP, SKP, COM};
-                        txdatak <= 8'b00001111;
-                        sending_SKP <= 1'b1;
+                        // No SKP OS to send
+                        txdata               <= {DATA_WIDTH{1'b0}};
+                        txdatak              <= {DATA_BYTES{1'b0}};
+                        sending_SKP          <= 1'b0;
+                        number_of_queued_SKP <= 4'b0;
                     end
+                end else if (SKP_count_inc) begin
+                    txdata               <= {DATA_WIDTH{1'b0}};
+                    txdatak              <= {DATA_BYTES{1'b0}};
+                    sending_SKP          <= 1'b0;
+                    number_of_queued_SKP <= number_of_queued_SKP + 1'b1;
                 end else begin
-                    txdata <= {DATA_WIDTH{1'b0}};
-                    txdatak <= {DATA_BYTES{1'b0}};
-                    sending_SKP <= 1'b0;
+                    txdata               <= {DATA_WIDTH{1'b0}};
+                    txdatak              <= {DATA_BYTES{1'b0}};
+                    sending_SKP          <= 1'b0;
+                    number_of_queued_SKP <= number_of_queued_SKP;
                 end
             end
-        end else if (DATA_WIDTH == 32) begin
+        end else begin
+            localparam NUMBER_OF_STEP = SKP_WIDTH / DATA_WIDTH;
+            reg [$clog2(NUMBER_OF_STEP):0] step_count;
+
             always @(posedge clk or posedge reset) begin
                 if (reset) begin
-                    txdata <= {DATA_WIDTH{1'b0}};
-                    txdatak <= {DATA_BYTES{1'b0}};
-                    sending_SKP <= 1'b0;
+                    txdata               <= {DATA_WIDTH{1'b0}};
+                    txdatak              <= {DATA_BYTES{1'b0}};
+                    sending_SKP          <= 1'b0;
+                    number_of_queued_SKP <= 4'b0;
+                    step_count           <= 0;
                 end else if (SKP_send_enable) begin
-                    txdata <= {SKP, SKP, SKP, COM};
-                    txdatak <= 4'b1111;
-                    sending_SKP <= 1'b1;
+                    if (step_count < (NUMBER_OF_STEP - 1)) begin
+                        txdata               <= SKP_OS[step_count*DATA_WIDTH - 1 +: DATA_WIDTH];
+                        txdatak              <= {DATA_BYTES{1'b1}};
+                        sending_SKP          <= 1'b1;
+                        if (SKP_count_inc)
+                            number_of_queued_SKP <= number_of_queued_SKP + 1'b1;
+                        else
+                            number_of_queued_SKP <= number_of_queued_SKP;
+                        step_count           <= step_count + 1'b1;
+                    end else if (step_count == (NUMBER_OF_STEP - 1)) begin
+                        txdata               <= SKP_OS[step_count*DATA_WIDTH - 1 +: DATA_WIDTH];
+                        txdatak              <= {DATA_BYTES{1'b1}};
+                        sending_SKP          <= 1'b1;
+                        if (SKP_count_inc)
+                            number_of_queued_SKP <= number_of_queued_SKP;
+                        else
+                            number_of_queued_SKP <= number_of_queued_SKP - 1'b1;
+                        step_count           <= 0;
+                    end
                 end else begin
-                    txdata <= {DATA_WIDTH{1'b0}};
-                    txdatak <= {DATA_BYTES{1'b0}};
-                    sending_SKP <= 1'b0;
+                    txdata               <= {DATA_WIDTH{1'b0}};
+                    txdatak              <= {DATA_BYTES{1'b0}};
+                    sending_SKP          <= 1'b0;
+                    number_of_queued_SKP <= number_of_queued_SKP;
+                    step_count           <= 0;
                 end
             end
         end
     endgenerate
 
-    assign SKP_count_inc = s_count == COUNT_MIN;
+    assign SKP_count_inc   = s_count == COUNT_MIN;
     assign SKP_send_enable = (number_of_queued_SKP != 4'b0) && (sending_data == 1'b0) && (sending_OS == 1'b0) && (sending_IDLE == 1'b0);
 
 endmodule
